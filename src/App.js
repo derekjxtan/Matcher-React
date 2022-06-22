f35cca4919703240eb5a5d603eae927adbd14d67
Derek Tan <derek.tan.jing.xu@gmail.com>
2022-06-22 20:01:34 +0800

    Home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import './App.css';
 import Header from './components/header';
 import Footer from './components/footer';
+import Home from './components/home';
 
 // map the store state to props so that components can access them
 function mapStateToProps(state) {
@@ -17,12 +18,14 @@ const mapDispatchToProps = (dispatch) => {
 class App extends Component {
   componentDidMount() {
     // for functions to be called when app component is mounted
+    document.body.style.backgroundColor = 'black';
   };
 
   render() {
     return (
       <div>
         <Header />
+        <Home />
         <Footer />
       </div>
     );
---
+++ src/components/home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <h1>Welcome to Matcher.</h1>
+                <p>
+                    The purpose of this project is to provide a service for people to match 
+                    1 set of things to another set based on others input. Examples where this 
+                    can be used include: duty scheduling, issuing items based on preferences, 
+                    assigning things to people, or any other applications that you can think 
+                    of.
+                </p>
+            </div>
+            <div className='row'>
+                <h2>If you're a Matcher:</h2>
+                <p>
+                    Login or Register to start creating a new match. This is to ensure that 
+                    only you can opt to generate the results.
+                </p>
+                <p>
+                    Click on "Create New Match" to start creating a new Match for others to 
+                    submit their inputs. If you're here to view the results of a Match you 
+                    already created, click on "Matches" to view your Matches.
+                </p>
+            </div>
+            <div className='row'>
+                <h2>If you're a Matchee:</h2>
+                <p>
+                    Click on "Submit Response" in order to submit your inputs for a created 
+                    match.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
